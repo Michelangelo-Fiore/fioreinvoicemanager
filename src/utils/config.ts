15bf6b8c9d52155cc3ea5202c84860bd814e3b6b
Chukwuebuka <chukwuebuka.tech@gmail.com>
2025-09-26 22:43:48 +0100

    feature: get clients list in the dashboard 1.0

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,14 +5,21 @@ import Cookies from "js-cookie";
 /** URL for API - PRODUCTION */
 // const frontendUrl = "https://fioreinvoicemanager.web.app";
 //
-const apiUrl = "https://fioreinvoicemanager.onrender.com";
-const frontendUrl = "https://fioreinvoicemanager.vercel.app/";
-const backendUrl = "https://fioreinvoicemanager.onrender.com";
+// const apiUrl = "https://fioreinvoicemanager.onrender.com";
+// const frontendUrl = "https://fioreinvoicemanager.vercel.app/";
+// const backendUrl = "https://fioreinvoicemanager.onrender.com";
 
 /** URL for API - LOCAL */
 // const apiUrl = "http://localhost:4000";
 // const frontendUrl = "http://localhost:3000";
 // const backendUrl = "https://fioreinvoicemanager.onrender.com";
+
+/** URL for API - LOCAL AND ON VERCEL ENV */
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000"; // backend
+const frontendUrl =
+	process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
+const backendUrl =
+	process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000";
 
 /** API BASE URL */
 const apiBaseUrl = `${apiUrl}/api`;
